c32bb832a954c1565919ebe2379c3372c19fa4ad
davisuga <daviciencia1@gmail.com>
2023-04-11 10:19:30 -0300

    Partial truffle translator and evaluator

---
+++ src/main/scala/AstToTruffle.scala
@@ -0,0 +1,19 @@
+package simplelang
+
+import Ast._
+
+def toTruffleNode(ast: Expr): SimpleLangNode = ast match {
+  case Expr.IntLit(value) => IntLitNode(value)
+  case Expr.If(cond, thenBranch, elseBranch) =>
+    IfNode(
+      toTruffleNode(cond),
+      toTruffleNode(thenBranch),
+      toTruffleNode(elseBranch)
+    )
+  case Expr.Let(name, value, body) =>
+    LetNode(name, toTruffleNode(value), toTruffleNode(body))
+  case Expr.Ident(name)      => IdentNode(name)
+  case Expr.Fn(params, body) => FnNode(params, toTruffleNode(body))
+  case Expr.Call(fn, args) =>
+    CallNode(toTruffleNode(fn), args.map(toTruffleNode))
+}
--- src/main/scala/Truffle.scala
+++ src/main/scala/Truffle.scala
@@ -1,11 +1,78 @@
+package simplelang
+
+import com.oracle.truffle.api.frame.VirtualFrame
 import com.oracle.truffle.api.nodes.Node
 
-abstract class ExpressionNode extends Node {}
+trait SimpleLangNode {
+  def execute(frame: VirtualFrame): AnyRef
+}
 
-case class AddNode(left: ExpressionNode, right: ExpressionNode)
-    extends ExpressionNode {}
+case class IntLitNode(value: Int) extends SimpleLangNode {
+  override def execute(frame: VirtualFrame): AnyRef = value.asInstanceOf[AnyRef]
+}
 
-case class SubNode(left: ExpressionNode, right: ExpressionNode)
-    extends ExpressionNode {}
+case class IfNode(
+    cond: SimpleLangNode,
+    thenBranch: SimpleLangNode,
+    elseBranch: SimpleLangNode
+) extends SimpleLangNode {
+  override def execute(frame: VirtualFrame): AnyRef = {
+    val condValue = cond.execute(frame).asInstanceOf[Boolean]
+    if (condValue) thenBranch.execute(frame) else elseBranch.execute(frame)
+  }
+}
 
-case class IntLiteralNode(value: Int) extends ExpressionNode {}
+case class LetNode(name: String, value: SimpleLangNode, body: SimpleLangNode)
+    extends SimpleLangNode {
+  override def execute(frame: VirtualFrame): AnyRef = {
+    val valueEvaluated = value.execute(frame)
+    val newFrame = frame.materialize().copy()
+    newFrame.setObject(
+      newFrame.getFrameDescriptor.findOrAddFrameSlot(name),
+      valueEvaluated
+    )
+    body.execute(newFrame)
+  }
+}
+
+case class IdentNode(name: String) extends SimpleLangNode {
+  override def execute(frame: VirtualFrame): AnyRef = {
+    frame.getValue(frame.getFrameDescriptor.findFrameSlot(name))
+  }
+}
+
+case class FnNode(params: List[String], body: SimpleLangNode)
+    extends SimpleLangNode {
+  override def execute(frame: VirtualFrame): AnyRef = {
+    new SimpleFunction(params, body, frame.materialize())
+  }
+}
+
+case class CallNode(fn: SimpleLangNode, args: List[SimpleLangNode])
+    extends SimpleLangNode {
+  override def execute(frame: VirtualFrame): AnyRef = {
+    val function = fn.execute(frame).asInstanceOf[SimpleFunction]
+    val newFrame = function.newFrame()
+    for ((param, argNode) <- function.params.zip(args)) {
+      newFrame.setObject(
+        newFrame.getFrameDescriptor.findOrAddFrameSlot(param),
+        argNode.execute(frame)
+      )
+    }
+    function.body.execute(newFrame)
+  }
+}
+
+class SimpleFunction(
+    val params: List[String],
+    val body: SimpleLangNode,
+    val lexicalScope: VirtualFrame
+) {
+  def newFrame(): VirtualFrame = {
+    val newFrame = lexicalScope.copy()
+    for (param <- params) {
+      newFrame.getFrameDescriptor.findOrAddFrameSlot(param)
+    }
+    newFrame
+  }
+}
