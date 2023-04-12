6c3ffad1680090d2db36b2d2adb0a0727d89fc40
davisuga <daviciencia1@gmail.com>
2023-04-11 21:15:38 -0300

    code in source for faster repl

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,12 @@
 package simplelang
-@main def hello: Unit =
-  println("Hello world!")
-  println(msg)
 
-def msg = "I was compiled by Scala 3. :)"
+val src = """
+// true
+// false
+let add x y = x + y in add 1 2
+"""
+
+import simplelang.Parser.parseInput
+@main def hello =
+  val lines = scala.io.Source.fromFile("ex.miniml").mkString
+  print(parseInput(src))
